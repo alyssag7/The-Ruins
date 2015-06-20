7f701d2f0d025f5780389cffb278b2035c996309
Aaron Aquino <Aaron@rescomp-14-299664.stanford.edu>
2015-06-20 10:08:23 -0700

    Snarf and solution are now publication ready.

--- Reorder SNARF.js
+++ Reorder SNARF.js
@@ -1,23 +1,21 @@
 /*
  * Write a function that takes a queue of integers that are already sorted by absolute value,
  * and modifies it so that the integers are sorted normally. You are not allowed to declare any
- * data structures other than a single Stack<int>.
+ * data structures other than a single stack of integers.
  */
 
 
-// TROLLOLOLOLOLOL
+function reorder(numbers) {
+	var stack = [];
 
- var nums = [-1, 1, 3, -4, 7, -7, 7, 10];
-
-
- function reorder(numbers) {
- 	var stack = [];
-
- 	// TODO: Complete your implementation of this function.
- }
+	// TODO: Complete your implementation of this function.
+}
 
 
- reorder(nums);
- for (var j = 0; j < nums.length; j++) {
- 	debug(nums[j]);
- }
+
+// Testing code to ensure reorder works!
+var nums = [0, -1, 1, 3, -5, 7, -7, 7, 13, 22, 22, 33, -50];
+reorder(nums);
+for (var j = 0; j < nums.length; j++) {
+	debug(nums[j]);
+}
